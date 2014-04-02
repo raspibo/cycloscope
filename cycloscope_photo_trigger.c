b91afd31de234177f52444e15e0c64a70134bfa3
dcast78 <castellari@alice.it>
2014-04-02 23:04:55 +0200

    Alcuni aggiustamenti al codice per mandare la cpu in sleep e ridurre i consumi, disabilitato temporaneamente uso timer_1

--- cycloscope_photo_trigger.c
+++ cycloscope_photo_trigger.c
@@ -9,6 +9,12 @@
 volatile int x=0;
 volatile uint8_t tot_overflow;
 
+void initInterrupt0(void) {                    //Imposta i registri per abilitare Interrupt sul pin INT0
+ EIMSK |= (1 << INT0);
+ EICRA |= (1 << ISC00);
+ sei();
+}
+
 ISR(INT0_vect) {                                //Imposta la routine da eseguire quando si riceve una variazione solo pin INT0
  if (bit_is_set(BUTTON_PIN, BUTTON)) {
   LED_PORT |= (1 << LED1);
@@ -16,44 +22,22 @@ ISR(INT0_vect) {                                //Imposta la routine da eseguire
  } else {
   LED_PORT &= ~(1 << LED1);
  }
-}
-
-ISR(TIMER1_OVF_vect) {                         //Imposta il timer su overflow di TIMER1
-    tot_overflow++;
-    if (tot_overflow >= 10 && x>=10) {
-      LED_PORT ^= (1 << 0);
-      tot_overflow = 0;   
-      x=0;
-    }
-}
-
-void initInterrupt0(void) {                    //Imposta i registri per abilitare Interrupt sul pin INT0
- EIMSK |= (1 << INT0);
- EICRA |= (1 << ISC00);
- sei();
-}
-
-void timer1_init(void) {                       //Imposta i registri per abilitare Interrupt su Timer1
-    TCCR1B |= (1 << CS11);
-    TCNT1 = 0;
-    TIMSK1 |=  (1<<TOIE1);
-    sei();
-    tot_overflow = 0;
+ if (x>=10) {                                  //Impostare a 238 per i test su strada//
+  LED_PORT |= (1 << LED0);
+  _delay_ms(100);
+  LED_PORT &= ~(1 << LED0);
+  x=0;
+ }
 }
 
 int main(void) {
-  LED_DDR = 0xff;                                   /* all LEDs active */
-  BUTTON_PORT |= (1 << BUTTON);                              /* pullup */
-  timer1_init();
+  LED_DDR = 0xff;                              /* all LEDs active */
+  BUTTON_PORT |= (1 << BUTTON);                /* pullup */
   initInterrupt0();
-  set_sleep_mode (SLEEP_MODE_PWR_DOWN);             /* Queste 3 istruzioni abilitano il modo in basso consumo della CPU */ 
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);        /* Queste 3 istruzioni abilitano il modo in basso consumo della CPU */ 
   sleep_enable();
-  sleep_cpu ();  
   while (1) {
-  _delay_ms(100);
-   if (x>=10) {
-    LED_PORT |= (1 << LED0);
-   }
+  sleep_cpu ();  
   }                                                  
-  return (0);                            /* This line is never reached */
+  return (0);                                  /* This line is never reached */
 }
