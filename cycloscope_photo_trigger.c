d557e2629f061ffc0fa0348dcfdcdfb804ab49c7
dcast78 <castellari@alice.it>
2014-04-07 18:05:34 +0200

    Version for hardware test

--- cycloscope_photo_trigger.c
+++ cycloscope_photo_trigger.c
@@ -4,6 +4,9 @@ Based on a description of: http://benlo.com/msp430/GoProController.html
 Reimplemented with Atmega328P 
 This code shoot a photo every SAMPLE_DIST meters
 The mcu go in sleep mode to reduce power consumption and wake up every .25 seconds with watchdog
+
+Compile and upload with: 
+rm *.hex *.elf ; make &&  avrdude -p m328p -c usbasp -U flash:w:cycloscope_photo_trigger.hex 
 */
 
 #include <avr/io.h>
@@ -16,7 +19,7 @@ The mcu go in sleep mode to reduce power consumption and wake up every .25 secon
 
 #define WHEEL_CIRC       67  //  Circonferenza della ruota in cm
 #define SAMPLE_DIST      500 //  Distanza in metri tra due foto
-#define SAMPLE_COUNT     238 //  Calcolato come SAMPLE_DIST/WHEEL_CIRC
+#define SAMPLE_COUNT     5 //  Calcolato come SAMPLE_DIST/WHEEL_CIRC
 
 #define GOPRO_WAKE_START 0    //   waiting for next cycle
 #define GOPRO_WAKE_STOP  1    //   button down to start
@@ -39,10 +42,12 @@ void initInterrupt0(void) {                    //Imposta i registri per abilitar
 
 ISR(INT0_vect) {                                //Imposta la routine da eseguire quando si riceve una variazione solo pin INT0
  if (bit_is_set(BUTTON_PIN, BUTTON)) {
-  LED_PORT |= (1 << LED1);
+  //LED_PORT |= (1 << LED1);
+  PORTC |= (1 << 3);
   x=x+1;
  } else {
-  LED_PORT &= ~(1 << LED1);
+//  LED_PORT &= ~(1 << LED1);
+  PORTC &= ~(1 << 3);
  }
  if (x>=SAMPLE_COUNT) {                                  //Impostare a 238 per i test su strada//
  LED_PORT &= ~(1 << LED0);
@@ -83,6 +88,7 @@ void enterSleep(void)
 
 int main(void) {
  LED_DDR = 0xff;                              /* all LEDs active */
+ DDRC = 0xff;                                 /*portc tutto out*/
  //  LED_PORT ^= (1 << LED0); //toggle
  BUTTON_PORT |= (1 << BUTTON);                /* pullup */
  initInterrupt0();
