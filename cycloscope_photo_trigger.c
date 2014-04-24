08ce20e66814b5515780e0bce6b3b42860d1cb48
dcast78 <dcast78@gmail.com>
2014-04-24 07:08:53 +0200

    Aggiunti commenti e reset contatore giri
    Aggiunti commenti su e65a8a9a4194fd8f001350799e9fec447bf18c4c e reset contatore giri sullo startup

--- cycloscope_photo_trigger.c
+++ cycloscope_photo_trigger.c
@@ -6,71 +6,72 @@ This code shoot a photo every SAMPLE_DIST meters
 The mcu go in sleep mode to reduce power consumption and wake up every .25 seconds with watchdog
 
 Compile and upload with: 
-rm *.hex *.elf ; avr-gcc -mmcu=atmega328 -DF_CPU=1000000UL -DBAUD=9600 -Os -I. -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums  -Wall -Wstrict-prototypes -g -ggdb -ffunction-sections -fdata-sections -Wl,--gc-sections -Wl,--relax -std=gnu99 cycloscope_photo_trigger.c     --output cycloscope_photo_trigger.elf && avr-objcopy -R .eeprom -O ihex cycloscope_photo_trigger.elf cycloscope_photo_trigger.hex &&  avrdude -p m328p -c usbasp -U flash:w:cycloscope_photo_trigger.hex 
 rm *.hex *.elf ; make &&  avrdude -p m328p -c usbasp -U flash:w:cycloscope_photo_trigger.hex 
 */
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "pinDefines.h"
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
 
 #define WHEEL_CIRC       67  //  Circonferenza della ruota in cm
 #define SAMPLE_DIST      500 //  Distanza in metri tra due foto
-#define SAMPLE_COUNT     238 //  Calcolato come SAMPLE_DIST/WHEEL_CIRC
+#define SAMPLE_COUNT     20 //  Calcolato come SAMPLE_DIST/WHEEL_CIRC
 
 #define GOPRO_WAKE_START 0    //   waiting for next cycle
 #define GOPRO_WAKE_STOP  1    //   button down to start
 #define GOPRO_SHUT_START 32   //   waiting for camera to take pic
 #define GOPRO_SHUT_END   56   //   button down to stop
 
-volatile int x=0;
+volatile int giri=0;
 volatile uint8_t tot_overflow;
 
 volatile int f_wdt=1;
 
+static int state;
 static int time;
 
 void initInterrupt0(void) {                    //Imposta i registri per abilitare Interrupt sul pin INT0
- EIMSK |= (1 << INT0);
- EICRA |= (1 << ISC00);
- sei();
+ EIMSK |= (1 << INT0);                         /* enable INT0 */
+ EICRA |= (1 << ISC00);                        /* trigger when button changes */
+ sei();                                        /* set (global) interrupt enable bit */
 }
 
 ISR(INT0_vect) {                                //Imposta la routine da eseguire quando si riceve una variazione solo pin INT0
- if (bit_is_set(PIND, PD2)) {
-  //PORTB |= (1 << LED1);
-  PORTC |= (1 << 3);
-  x=x+1;
+ if (bit_is_set(BUTTON_PIN, BUTTON)) {
+  //LED_PORT |= (1 << LED1);
+  PORTC |= (1 << 3);                            //Accensione del led verde port c pin 3 quando si verifica l'interrupt ed il pin è alto
+  giri=giri+1;                                  //incrementa il contatore di giri della ruota
  } else {
-//  PORTB &= ~(1 << LED1);
-  PORTC &= ~(1 << 3);
+//  LED_PORT &= ~(1 << LED1);
+  PORTC &= ~(1 << 3);                           //Spegne il  led verde port c pin 3 quando si verifica l'interrupt ed il pin è basso
  }
- if (x>=SAMPLE_COUNT) {                                  //Impostare a 238 per i test su strada//
- PORTB &= ~(1 << PB0);
-  time=GOPRO_WAKE_START;
-  wdt_reset();
+ if (giri>=SAMPLE_COUNT) {                      //Raggiunta la quantità di giri per lo scatto. Impostare a 238 per i test su strada//
+ LED_PORT &= ~(1 << LED0);                      //Accensione della telecamera e del led giallo (B0) il pin va a massa
+  time=GOPRO_WAKE_START;                        //Azzera il conteggio di interrupt di watchdog per il calcolo del tempo
+  wdt_reset();                                  //Azzera il watchdog per un calcolo preciso del tempo
  }
 }
 
 ISR(WDT_vect)
 {
-  sleep_disable();          // Disable Sleep on Wakeup
-  time++;
- if (time==GOPRO_WAKE_STOP && x>=SAMPLE_COUNT) {
-   PORTB |= (1 << PB0); 
+  sleep_disable();                              //Disable Sleep on Wakeup
+  time++;                                       //Incrementa il contatore di tempo dopo il risveglio del watchdog
+ if (time==GOPRO_WAKE_STOP && giri>=SAMPLE_COUNT) {  //Se GOPRO_WAKE_STOP(=1) sono passati 250 msecondi la gopro è accesa
+   LED_PORT |= (1 << LED0);                     //Spengo il led a vcc il pin B0 (spegne il led giallo), la go pro è accesa
   }
-  if (time==GOPRO_SHUT_START && x>=SAMPLE_COUNT) {
-   PORTB &= ~(1 << PB0);
+  if (time==GOPRO_SHUT_START && giri>=SAMPLE_COUNT) {//GOPRO_SHUT_START(32) raggiunti 32 conteggi di watchdog la foto è scattata
+   LED_PORT &= ~(1 << LED0);                    //Spengo la go pro portando a massa il pin B0(il led giallo si accende)
   }
-  if (time>=GOPRO_SHUT_END && x>=SAMPLE_COUNT) {
-   PORTB |= (1 << PB0);
-   time=GOPRO_WAKE_START;
-   x=0;
+  if (time>=GOPRO_SHUT_END && giri>=SAMPLE_COUNT) {  //dopo 3 secondi finisce la sequenza di shutdown della go pro
+   LED_PORT |= (1 << LED0);                     //Spengo il led a vcc il pin B0 (spegne il led giallo), la go pro è spenta
+   time=GOPRO_WAKE_START;                       //Azzera il conteggio di interrupt di watchdog per il calcolo del tempo
+   giri=0;                                      //Azzera il contatore di giri
   }
-  sleep_enable();           // Enable Sleep Mode
+  sleep_enable();                               // Enable Sleep Mode
 }
 
 void enterSleep(void)
@@ -79,30 +80,30 @@ void enterSleep(void)
  sleep_enable();
  sleep_mode();                          /* Now enter sleep mode. */
  /* The program will continue from here after the WDT timeout*/
- sleep_disable(); /* First thing to do is disable sleep. */
+ sleep_disable();                       /* First thing to do is disable sleep. */
  /* Re-enable the peripherals. */
  power_all_enable();
 }
 
 
 int main(void) {
- DDRB = 0x01;                                 /*portb out led pin 12 go pro*/
- DDRB = 0x01;                              /*pin 12 high */
+ DDRB = 0x01;                                 /*setta come uscita solo il pin b0 a cui è collegato il led e ed il pin 12 di go pro*/
+ LED_DDR = 0x01;                              /*setta alto il pin 12, collegato a led e vcc */
+ DDRC = 0x08;                                 /*portc pin 3 out pin debug, collegato a led (varde) e poi a vcc*/
+ PORTC &= ~(1 << 3);                          /*setta alto pin 3 port c, led spento*/
  DDRC = 0x08;                                 /*portc out pin debug*/
- PORTC &= ~(1 << 3);
- DDRC = 0x08;                                 /*portc out pin debug*/
- //  PORTB ^= (1 << PB0); //toggle
- PORTD |= (1 << PD2);                /* pullup */
- initInterrupt0();
- /* Clear the reset flag. */
- MCUSR &= ~(1<<WDRF);
- /* In order to change WDE or the prescaler, we need to set WDCE (This will allow updates for 4 clock cycles). */
- WDTCSR |= (1<<WDCE) | (1<<WDE);
+ //  LED_PORT ^= (1 << LED0); //toggle
+ BUTTON_PORT |= (1 << BUTTON);                /*pullup sul pin INT0, collegato a sensore magnetico*/
+ initInterrupt0();                            /*inizializza le variabili per abilitare interrutp su pin sensore magnetico*/
+ MCUSR &= ~(1<<WDRF);                         /*Clear the reset flag. */
+ WDTCSR |= (1<<WDCE) | (1<<WDE);              /*In order to change WDE or the prescaler, we need to set WDCE (This will allow updates for 4 clock cycles). */
  //WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */  /* set new watchdog timeout prescaler value */
- WDTCSR = 1<<WDP2; /* 250 milliseconds */
- WDTCSR |= _BV(WDIE);  /* Enable the WD interrupt (note no reset). */
+ WDTCSR = 1<<WDP2;                            /*WDT interrupt every 250 milliseconds */
+ WDTCSR |= _BV(WDIE);                         /*Enable the WD interrupt (note no reset). */
 
- PORTB |= (1 << PB0); //Spegne il led
+ LED_PORT |= (1 << LED0);                     //Spegne il led giallo nel caso fosse rimasto acceso
+
+ giri=0;                                      //Azzera il contatore di giri 
 
  while (1) {
    enterSleep();
